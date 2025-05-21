f9f833c57316508acf5eebeaeb310d229365bf99
Kar-aud <KAREN.VERAUD@latoile.em-lyon.com>
2025-05-21 12:44:22 +0200

    rendu

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json')
+ .then(response => response.json()) 
+    .then(data => {
+        let container = document.getElementById("container");
+        console.log(data);)
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        //c'est un échec pour moi. mais je suis quand même venue affronter la bête.
+        // merci pour votre soutient et votre aide. Spécialement Joris qui a eu un vain espoir de pouvoir m'éclairer.
